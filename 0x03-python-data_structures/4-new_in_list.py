4354d145b49a3627bfe0119b9a728a80ecbd98aa
bemnet <bemnetabebe1@gmail.com>
2024-03-18 13:05:00 -0700

    replace

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def copy_list(my_list):
+    new_list = []
+    if my_list:
+        for i in range(len(my_list)):
+            new_list.append(my_list[i])
+        return new_list
+def new_in_list(my_list, idx, element):
+    if my_list:
+        _copy = copy_list(my_list)
+        if idx < 0 or idx > len(my_list):
+            return 'None'
+        else:
+            _copy[idx] = element
+            return _copy 
+
