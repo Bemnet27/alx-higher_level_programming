332548402def975bf08c42bd1e0b2501dfc779bc
bemnet <bemnetabebe1@gmail.com>
2024-03-18 13:09:14 -0700

    error

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-def copy_list(my_list):
-    new_list = []
-    if my_list:
-        for i in range(len(my_list)):
-            new_list.append(my_list[i])
-        return new_list
-
-
 def new_in_list(my_list, idx, element):
-    if my_list:
-        _copy = copy_list(my_list)
-        if idx < 0 or idx > len(my_list):
-            return 'None'
-        else:
-            _copy[idx] = element
-            return _copy
+    copy = my_list.copy()
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list.copy()
+    else:
+        copy[idx] = element
+        return copy
