5a5b3c558813c5d8fcd42ee0fa16e418ace4eee0
bemnet <bemnetabebe1@gmail.com>
2024-04-03 06:10:43 -0700

    size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""define a class square."""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size):
+        """Initializes a square
+
+        Args:
+            size (int): the size of the new square.
+            """
+        self.__size = size
